f54d555c9ecd70c14dcc931755dc34497c95215d
B3none <ablackham2000@gmail.com>
2019-02-16 16:28:10 +0000

    Added initial sample plugin.

---
+++ src/main/java/b3none/thegiftoflife/SampleDebugCommand.java
@@ -0,0 +1,29 @@
+package b3none.the-gift-of-life.sample;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Handler for the /debug sample command.
+ * @author SpaceManiac
+ */
+public class SampleDebugCommand implements CommandExecutor {
+    private final SamplePlugin plugin;
+
+    public SampleDebugCommand(SamplePlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            plugin.setDebugging(player, !plugin.isDebugging(player));
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
